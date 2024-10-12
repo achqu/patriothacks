5250fbe0c7f9b3eb0067e312a9d41c323937dc0b
meiiqu <acmqux@gmail.com>
2024-10-12 17:25:01 -0400

    landing page

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, Text, View } from 'react-native';
-import {Link} from 'expo-router';
+import {Link, Redirect, router} from 'expo-router';
 import { SafeAreaView} from 'react-native-safe-area-context';
 import {images} from '../constants'
 import {CustomButton} from '../components';
@@ -10,7 +10,7 @@ export default function App() {
     <SafeAreaView className = "bg-primary h-full">
       <ScrollView contentContainerStyle={{height: '100%'}}>
 
-        <View className= "w-full justify-center items-center h-full px-4">
+        <View className= "w-full justify-center items-center min-h-[85vh] px-4">
             <Image 
               source={images.logo}
               className="w-[130px] h-[84px]"
@@ -40,12 +40,18 @@ export default function App() {
               intro description here idk
             </Text>
 
-            <CustomButton />
+            <CustomButton 
+              title="Continue with Email"
+              handlePress={() => router.push('/sign-in')}
+              containerStyles="w-full mt-7"
+            />
 
 
         </View>
 
       </ScrollView>
+
+      {/*<StatusBar backgroundColor="#161622" style="light" />*/}
     </SafeAreaView>
   );
 }
